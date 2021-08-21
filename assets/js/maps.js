aa4623c4e3b7da3e0ad687135e8125d76ef87af1
RuaOCuinn <ruaocuinneagain@yahoo.com>
2021-08-21 10:23:40 +0000

    Made slight changes to maps.js.

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -1,16 +1,16 @@
 function initMap() {
     var map = new google.maps.Map(document.getElementById("map"), {
-        zoom: 3,
+        zoom: 6,
         center: {
-            lat: 46.619261,
-            lng: -33.134766
+            lat: 53.41291,
+            lng: -8.24389
         }
     });
 
     var labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
     var locations = [
-        { lat: 55.240833, lng: -6.511667 },
+        [{ lat: 55.240833, lng: -6.511667 }, "Giants Causeway"],
         { lat: 53.579722, lng: -6.611944 },
         { lat: 53.694712, lng: -6.475492 },
         { lat: 53.760057, lng: -9.659387 }
