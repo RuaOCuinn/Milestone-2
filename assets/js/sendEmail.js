403b0a726603fcb237b691e565b4e530555f31e0
RuaOCuinn <ruaocuinneagain@yahoo.com>
2021-08-21 17:20:44 +0000

    Finished sendEmail.js.

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -1,1 +1,16 @@
-console.log("Hello");
+// From Code institue lesson for Sending Emails
+function sendMail(contactForm) {
+    emailjs.send("gmail", "Mythical Locations", {
+        "from_name": contactForm.name.value,
+        "from_email": contactForm.emailaddress.value,
+        "message": contactForm.contact.value
+    })
+    .then(
+        function(response) {
+            console.log("SUCCESS", response);
+        },
+        function(error) {
+            console.log("FAILED", error);
+        });
+          return false;  // To block from loading a new page
+}
